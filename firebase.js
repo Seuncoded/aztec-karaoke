1f45541f23f612b8d86baea1241cf922d50fa48e
seuncoded <seuncodedmedia@gmail.com>
2025-09-10 17:59:34 +0100

    Fix key Issues

--- firebase.js
+++ firebase.js
@@ -3,13 +3,14 @@ import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebas
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
 import { getStorage } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-storage.js";
 
+// ✅ Load values from env.js
 const firebaseConfig = {
   apiKey: window.env.NEXT_PUBLIC_FIREBASE_API_KEY,
   authDomain: window.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
   projectId: window.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
   storageBucket: window.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
   messagingSenderId: window.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: window.env.NEXT_PUBLIC_FIREBASE_APP_ID
+  appId: window.env.NEXT_PUBLIC_FIREBASE_APP_ID,
 };
 
 const app = initializeApp(firebaseConfig);
