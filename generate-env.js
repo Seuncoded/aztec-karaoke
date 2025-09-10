34aec6b609d02577504c9f63b67d2efcda92d293
seuncoded <seuncodedmedia@gmail.com>
2025-09-10 18:27:25 +0100

    Remove root env.js

--- generate-env.js
+++ generate-env.js
@@ -1,12 +1,16 @@
-const fs = require("fs");
+// generate-env.js
+import { writeFileSync } from "fs";
 
-const content = `window.env = {
+const envFile = `
+window.env = {
   NEXT_PUBLIC_FIREBASE_API_KEY: "${process.env.NEXT_PUBLIC_FIREBASE_API_KEY}",
   NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN: "${process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN}",
   NEXT_PUBLIC_FIREBASE_PROJECT_ID: "${process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID}",
   NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET: "${process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET}",
   NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID: "${process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID}",
   NEXT_PUBLIC_FIREBASE_APP_ID: "${process.env.NEXT_PUBLIC_FIREBASE_APP_ID}"
-};`;
+};
+`;
 
-fs.writeFileSync("./public/env.js", content);
+writeFileSync("./public/env.js", envFile);
+console.log("✅ env.js generated!");
